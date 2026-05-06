502be844359154196ed3271cb7e89b720f8b8dad
kacperlavender <kacper.kielbasa5000@gmail.com>
2026-05-06 20:50:19 +0200

    Restrukturyzacja kodu

--- main.py
+++ main.py
@@ -2,69 +2,78 @@ from collections import defaultdict
 import random
 import math
 
-def generate(start_char, length):
-    result = [start_char]
-    current = start_char
+class Markov:
+    def __init__(self, text):
+        self.text = text
+        self.probs = self._build_probs(text)
+    
+    def _build_probs(self, text):
+        counts = defaultdict(lambda: defaultdict(int))
 
-    for _ in range(length - 1):
-        if current not in probs:
-            break
+        for i in range(len(text) - 1):
+            a = text[i]
+            b = text[i + 1]
+            counts[a][b] += 1
 
-        next_chars = list(probs[current].keys())
-        weights = list(probs[current].values())
+        probs = {}
 
-        current = random.choices(next_chars, weights=weights, k=1)[0]
-        result.append(current)
+        for a, transition in counts.items():
+            total = sum(transition.values())
+            probs[a] = {b: cnt / total for b, cnt in transition.items()}
 
-    return ''.join(result)
+        return probs 
 
-def perplexity(test_text):
-    log_prob = 0.0
-    count = 0
+            
+    def generate(self, start_char, length):
+        result = [start_char]
+        current = start_char
 
-    for i in range(len(test_text) - 1):
-        a = test_text[i]
-        b = test_text[i + 1]
+        for _ in range(length - 1):
+            if current not in self.probs:
+                break
 
-        p = probs.get(a, {}).get(b, None)
+            next_chars = list(self.probs[current].keys())
+            weights = list(self.probs[current].values())
 
-        if p is None or p == 0:
-            p = 1e-10
-        
-        log_prob += math.log(p)
-        count += 1
-    return math.exp(-log_prob / count)
+            current = random.choices(next_chars, weights=weights, k=1)[0]
+            result.append(current)
+
+        return ''.join(result)
+
+    def perplexity(self, test_text):
+        log_prob = 0.0
+        count = 0
+
+        for i in range(len(test_text) - 1):
+            a = test_text[i]
+            b = test_text[i + 1]
+
+            p = self.probs.get(a, {}).get(b, None)
+
+            if p is None or p == 0:
+                p = 1e-10
+            
+            log_prob += math.log(p)
+            count += 1
+        return math.exp(-log_prob / count) if count else float('inf')
 
 def main():
-    text = open("pan_tadeusz.txt", encoding="utf-8").read()
-    chars = sorted(set(text))
+    with open("data/pan_tadeusz.txt", encoding="utf-8") as f:
+        text = f.read()
 
-    stoi = {ch: i for i, ch in enumerate(chars)}
-    itos = {i: ch for i, ch in enumerate(chars)}
+    
 
-    counts = defaultdict(lambda: defaultdict(int))
+    model = Markov(text)
 
-    for i in range(len(text) - 1):
-        a = text[i]
-        b = text[i + 1]
+    # seed = random.choice(list(model.probs.keys()))
+    seed = 'a'
+    generated = model.generate(seed, 50)
 
-        counts[a][b] += 1
+    print(f"[seed: {seed}]:\n {generated}")
 
-    probs = {}
-
-    for a in counts:
-        total = sum(counts[a].values())
-        probs[a] = {b: count/total for b, count in counts[a].items()}
-
-    for i in range(3):
-        seed = random.choice(list(probs.keys()))
-        print(f"[Seed: {seed}]: {generate(seed, 50)}")
-        print()
-
+    print(f"perplexity na korpusie: {model.perplexity(text):.2f}")
+    print(f"perplexity na 'Soplica ': {model.perplexity('Soplica '):.2f}")
+    print(f"perplexity na 'xyz123': {model.perplexity('xyz123'):.2f}")
 
 if __name__ == '__main__':
     main()
-
-    print(f"perplexity na korpusie: {perplexity(text):.2f}")
-    print(f"perplexity na 'Soplica ': {perplexity('Soplica '):.2f}")
-    print(f"perplexity na 'xyz123': {perplexity('xyz123'):.2f}")
